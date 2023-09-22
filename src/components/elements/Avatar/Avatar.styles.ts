ca4ca63e455c6bdee12954b5fdb01e5017d507ce
mtthdz <matt@mtthdz.com>
2023-09-21 22:48:34 -0400

    created avatar

---
+++ src/components/elements/Avatar/Avatar.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const SAvatar = StyleSheet.create({
+  view: {
+    backgroundColor: '#0000FF',
+    borderRadius: 50,
+    height: 20,
+    width: 20,
+  }
+})
+
+export default SAvatar;
---
+++ src/components/elements/Avatar/index.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import SAvatar from "./Avatar.styles";
+
+const Avatar = () => {
+  return <View style={SAvatar.view} />;
+}
+
+export default Avatar;
