aa111770655e84675a1f9fbc1cf8f596409a827d
mtthdz <matt@mtthdz.com>
2023-09-21 22:08:32 -0400

    created basic conversation dataset to map over

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import ChatMessage from './components/elements/ChatMessage';
 import Layout from './components/sections/Layout';
 
 function App(): JSX.Element {
+  const data = [
+    {message: 'hello'},
+    {message: 'my name is matt'},
+    {message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+  ]
+
   return (
     <SafeAreaView>
       <Layout>
         <Text>chat</Text>
-        <ChatMessage body='hello' />
+        <View style={styles.view}>
+          {data.map((item, index) => {
+            return <ChatMessage body={item.message} key={index}/>
+          })}
+        </View>
       </Layout>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+  }
+})
 
 export default App;
