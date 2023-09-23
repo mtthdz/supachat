52593d25dc6acef01fe47996b453ca711b8dcec6
mtthdz <matt@mtthdz.com>
2023-09-23 15:43:25 -0400

    react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import ChatMessage from './components/elements/ChatMessage';
-import Layout from './components/sections/Layout';
-import ChatTimestamp from './components/containers/ChatTimestamp';
-import { chatData } from './api/chatData';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft, faArrowUp, faPen } from '@fortawesome/free-solid-svg-icons';
+import RootNav from './navigation/RootNav';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+library.add(
+  faArrowUp,
+  faArrowLeft,
+  faPen,
+)
 
 function App(): JSX.Element {
-  let data = chatData;
-
   return (
-    <SafeAreaView>
-      <Layout>
-        <View style={styles.view}>
-          {data.map((item, index) => {
-            return <ChatMessage body={item.message} key={index}/>
-          })}
-          <ChatTimestamp />
-        </View>
-      </Layout>
-    </SafeAreaView>
+    <SafeAreaProvider>
+        <RootNav />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  view: {
-    flexDirection: 'column',
-    flexWrap: 'nowrap',
-  }
-})
 
 export default App;
