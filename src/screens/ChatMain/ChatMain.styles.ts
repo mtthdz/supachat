805e5ecdee8bbfc1413cf76472ef77e5e523e954
mtthdz <matt@mtthdz.com>
2023-09-23 15:46:49 -0400

    created main page for chat interface

---
+++ src/screens/ChatMain/ChatMain.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const SChatMain = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  flexParent: {
+    flex: 1,
+  }
+});
+
+export default SChatMain;
---
+++ src/screens/ChatMain/index.tsx
@@ -0,0 +1,36 @@
+import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native"
+import ChatForm from "../../components/sections/ChatForm"
+import ChatHeader from "../../components/sections/ChatHeader"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import ChatBody from "../../components/sections/ChatBody";
+import SChatMain from "./ChatMain.styles";
+
+const ChatMain = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom = 30,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}
+    >
+      <View style={SChatMain.container}>
+        <ChatHeader />
+          <KeyboardAvoidingView
+            style={SChatMain.flexParent}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={94}
+          >
+            <ChatBody />
+            <ChatForm />
+          </KeyboardAvoidingView>
+      </View>
+    </View>
+  );
+};
+
+export default ChatMain;
