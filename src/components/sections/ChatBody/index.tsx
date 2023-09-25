4a66bf0d2f53dd63ea3634b31e220e5bc946d565
mtthdz <matt@mtthdz.com>
2023-09-24 20:32:54 -0400

    fixed autoscroll to bottom of chat body on keyboard events

--- src/components/sections/ChatBody/index.tsx
+++ src/components/sections/ChatBody/index.tsx
@@ -1,4 +1,5 @@
-import { ScrollView, Text } from "react-native"
+import { useRef } from "react";
+import { Keyboard, ScrollView } from "react-native"
 import ChatMessage from "../../elements/ChatMessage"
 import SChatBody from "./ChatBody.styles";
 import Layout from "../Layout";
@@ -6,27 +7,45 @@ import { chatData } from "../../../api/chatData";
 import ChatTimestamp from "../../containers/ChatTimestamp";
 
 /**
- * TODO: add automatic scrolling to bottom of chat window
- * 
  * @returns JSX
  */
 const ChatBody = () => {
   const data = chatData;
 
+  /**
+   * Keyboard scroll functionality
+   * Notes:
+   * - we want the user to see the latest messages when opened
+   * - when keyboard is opened, KeyboardAvoidingView (ChatMain)
+   *   doesn't extend functionality to this sibling component
+   * - we've added a keyboard open listener to autoscroll down
+   * - (TS) useRef requires a type. Avoiding 'any', we can use
+   *   the actual type.
+   * https://stackoverflow.com/questions/64532946/adding-type-for-useref-on-scrollview-react-native
+   */
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  Keyboard.addListener("keyboardWillShow", () => {
+    scrollViewRef.current?.scrollToEnd();
+  })
+
   return (
-    <ScrollView
-      style={SChatBody.container}>
-      <Layout>
-        {
-          data.map((input, index) => {
-            return (
-              <ChatMessage body={input.message} key={index} />
-            );
-          })
-        }
-        <ChatTimestamp />
-      </Layout>
-    </ScrollView>
+      <ScrollView
+        style={SChatBody.container}
+        ref={scrollViewRef}
+        onContentSizeChange={() => {scrollViewRef.current?.scrollToEnd()}}
+      >
+        <Layout>
+          {
+            data.map((input, index) => {
+              return (
+                <ChatMessage body={input.message} key={index} />
+              );
+            })
+          }
+          <ChatTimestamp />
+        </Layout>
+      </ScrollView>
   );
 };
 
