2a4afd7272e5be25d99cd501c70483b22c36d684
mtthdz <matt@mtthdz.com>
2023-09-24 23:23:02 -0400

    basic api test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faArrowLeft, faArrowUp, faPen } from '@fortawesome/free-solid-svg-icons';
 import RootNav from './navigation/RootNav';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
+import { supabase } from './lib/supabase';
 
 library.add(
   faArrowUp,
@@ -13,6 +14,26 @@ library.add(
 )
 
 function App(): JSX.Element {
+  const [chatList, setChatList] = useState({});
+
+  
+  useEffect(() => {
+    const fetchChats = async () => {
+      let { data, error } = await supabase
+        .from('chat')
+        .select('*')
+
+      if (error) {
+        console.log(error);
+
+      } else if (data) {
+        console.log(data);
+        // setChatList(data);
+      }
+    }
+    fetchChats();
+  }, [])
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
