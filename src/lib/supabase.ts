699ad393c86ae5ac58d11e351027aa2297f6abe6
mtthdz <matt@mtthdz.com>
2023-09-25 23:33:38 -0400

    fixed dotenv

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-// import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 export const supabase = createClient(
-  'https://hrnylgxecwjrlbnqbodg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhybnlsZ3hlY3dqcmxibnFib2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUyNDIxMzEsImV4cCI6MjAxMDgxODEzMX0.eG04DbEGZbdPjnrcenuMXDBRdkh8xG-D96fWQNXdLbw',
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
   {
     auth: {
       storage: AsyncStorage,
