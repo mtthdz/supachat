6874e62e13a60d9fd8910459b1bdfa50edb38ec8
mtthdz <matt@mtthdz.com>
2023-09-25 23:28:49 -0400

    base supabase query, prop drill parent

--- src/screens/ChatHome/index.tsx
+++ src/screens/ChatHome/index.tsx
@@ -2,18 +2,50 @@ import { View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import MainHeader from "../../components/sections/MainHeader"
 import MainFeed from "../../components/sections/MainFeed"
-import SChatHome from "./ChatHome.styles"
+import { useEffect, useState } from "react"
+import { supabase } from "../../lib/supabase"
 
 
 /**
+ * TODO: fix type error (54)
+ * TODO: filter query to just user (currently no RLS)
+ * 
  * Notes:
- * Layout wrapper component within component themselves
- * so that border can extend to bounds of viewport
+ * - Layout wrapper component within component themselves
+ *   so that border can extend to bounds of viewport
+ * - fetch all chats for a specific user
  * 
  * @returns JSX
  */
 const ChatHome = () => {
   const insets = useSafeAreaInsets();
+  const [chatList, setChatList] = useState<object>([]);
+
+  const fetchChats = async () => {
+    let { data, error } = await supabase
+      .from('chat')
+      .select(`
+        *,
+        a:chat_user!user_a(user_name),
+        b:chat_user!user_b(user_name),
+        chat_message!id(
+          *,
+          user:chat_user!user_id(user_name)
+        )
+      `)
+
+    if (error) {
+      console.log(error);
+
+    } else if (data) {
+      setChatList(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchChats();
+    console.log(chatList);
+  }, [])
 
   return (
     <View 
@@ -26,7 +58,7 @@ const ChatHome = () => {
       }}
     >
       <MainHeader />
-      <MainFeed />
+      <MainFeed chatData={chatList} />
     </View>
   )
 }
