bef2ac9d48ff03c152ce4b0af58889a39369bc91
mtthdz <matt@mtthdz.com>
2023-09-25 23:29:29 -0400

    prop drill child, username presentation logic

--- src/components/sections/MainFeed/index.tsx
+++ src/components/sections/MainFeed/index.tsx
@@ -1,19 +1,36 @@
-import { ScrollView } from "react-native"
+import { FlatList, ScrollView, Text} from "react-native"
 import ChatCard from "../../containers/ChatCard"
 import SMainFeed from "./MainFeed.styles";
+import { reduxSelect } from "../../../types/reduxHooks";
 
-const MainFeed = () => {
+
+// TODO: replace this with supabase type
+type row = {
+  created_at: string
+  id: string
+  updated_at: string | null
+  user_a: string | null
+  user_b: string | null
+  a: {user_name: string},
+  b: {user_name: string},
+}
+
+type chatProps = {
+  chatData: row[]
+}
+
+const MainFeed = ({ chatData }: chatProps) => {
+  let data = chatData;
+  let user = reduxSelect(state => state.auth.username)
+
   return (
-    <ScrollView>
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-      <ChatCard />
-    </ScrollView>
+    <FlatList
+      data={data}
+      renderItem={({item, index}) => {
+        let otherUser = item.a.user_name === user ? item.b.user_name : user;
+        return <ChatCard name={otherUser} message={'hello there'} key={index}/>
+      }}
+    />
   );
 };
 
